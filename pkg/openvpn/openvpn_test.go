de5ec50e240675d251929e98533fb119f61c42c6
Lukas Hoehl <lukas.hoehl@stackit.de>
2024-06-12 12:30:38 +0200

    path-controller: restructure and add tests
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.de>

---
+++ pkg/openvpn/openvpn_test.go
@@ -0,0 +1,13 @@
+package openvpn
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestOpenVPN(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "OpenVPN Suite")
+}
