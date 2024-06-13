3161cef43cb6a593ebaaf9aa569e4ce629cad45b
Lukas Hoehl <lukas.hoehl@stackit.de>
2024-06-13 15:28:15 +0200

    log stacktrace only on panic
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.de>

--- pkg/utils/initrun.go
+++ pkg/utils/initrun.go
@@ -10,10 +10,12 @@ import (
 	"github.com/gardener/gardener/pkg/logger"
 	"github.com/go-logr/logr"
 	"github.com/spf13/cobra"
+	"go.uber.org/zap/zapcore"
 	"k8s.io/component-base/version"
 	"k8s.io/component-base/version/verflag"
 	"k8s.io/klog/v2"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 // InitRun initializes the run command by creating and settings a logger,
@@ -23,7 +25,7 @@ func InitRun(cmd *cobra.Command, name string) (logr.Logger, error) {
 
 	logLevel := "info"
 	logFormat := "text"
-	log, err := logger.NewZapLogger(logLevel, logFormat)
+	log, err := logger.NewZapLogger(logLevel, logFormat, zap.StacktraceLevel(zapcore.PanicLevel))
 	if err != nil {
 		return logr.Discard(), fmt.Errorf("error instantiating zap logger: %w", err)
 	}
