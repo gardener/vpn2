35b9ff6cbed4f21a0796cb69aa80518400a74e87
Axel Siebenborn <axel.siebenborn@sap.com>
2024-11-19 16:03:57 +0100

    Revert "Simplify config."
    This reverts commit 3abf68bf61216bf4215fda780b62e35f3920573a.

--- pkg/config/pathcontroller.go
+++ pkg/config/pathcontroller.go
@@ -5,6 +5,7 @@
 package config
 
 import (
+	"strings"
 
 	"github.com/caarlos0/env/v10"
 	"github.com/gardener/vpn2/pkg/network"
@@ -20,6 +21,9 @@ type PathController struct {
 	ServiceNetwork network.CIDR `env:"SERVICE_NETWORK"`
 }
 
+func (v PathController) PrimaryIPFamily() string {
+	return strings.Split(v.IPFamilies, ",")[0]
+}
 
 func GetPathControllerConfig(log logr.Logger) (PathController, error) {
 	cfg := PathController{}
