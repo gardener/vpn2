de7ad4482ed5d17096dcbc6911f506139fc960d6
Dominik Froehlich <dominik.froehlich@sap.com>
2025-12-16 14:36:11 +0100

    fix: move try variable out of ping func

--- cmd/vpn_client/app/pathcontroller/ping.go
+++ cmd/vpn_client/app/pathcontroller/ping.go
@@ -33,12 +33,15 @@ const echoPayload = "HELLO-R-U-THERE"
 
 func (p *icmpPinger) Ping(client net.IP) error {
 	var err error
-	for i := 1; i <= p.retries+1; i++ {
-		err = p.pingWithTimer(client, i)
+	tries := p.retries + 1
+	for try := 1; try <= tries; try++ {
+		err = p.pingWithTimer(client)
 		if err == nil {
 			break
 		}
-		if i == 1 {
+		p.log.Info("ping failed", "ip", client, "error", err, "try", fmt.Sprintf("%d/%d", try, tries))
+
+		if try == 1 {
 			go func() {
 				// send neighbor solicitation to speed up discovery the link-layer address of a neighbor
 				p.log.Info("sending neighbor solicitation", "ip", client.String())
@@ -54,8 +57,7 @@ func (p *icmpPinger) Ping(client net.IP) error {
 	return err
 }
 
-func (p *icmpPinger) pingWithTimer(client net.IP, try int) error {
-	tries := p.retries + 1
+func (p *icmpPinger) pingWithTimer(client net.IP) error {
 	timer := time.Now()
 	err := p.ping(client)
 
@@ -65,9 +67,8 @@ func (p *icmpPinger) pingWithTimer(client net.IP, try int) error {
 		} else {
 			var neterr net.Error
 			if errors.As(err, &neterr) && neterr.Timeout() {
-				err = fmt.Errorf("i/o timeout")
+				err = fmt.Errorf("i/o timeout after %dms", d.Milliseconds())
 			}
-			p.log.Info("ping failed", "ip", client, "duration", fmt.Sprintf("%dms", d.Milliseconds()), "error", err, "try", fmt.Sprintf("%d/%d", try, tries))
 		}
 	}
 	return err
